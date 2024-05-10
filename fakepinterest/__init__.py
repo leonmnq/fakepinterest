73b6b0efbfec0ccfa55a92c49d26c06ce5d5d817
leonmnq <leon.mnq@gmail.com>
2024-05-10 03:49:12 -0300

    sem criptografia

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -15,10 +15,10 @@ app = Flask(__name__)  # app nada mais é, que o nosso site, nossa aplicação
 #     link_banco = "sqlite:///comunidade.db"  # utiliza o banco de dados local, offline
 
 # PARA CRIAR AS TABELAS DO BANCO DE DADOS LÁ NO BANCO POSTEGRESQL DO RENDER, DESCOMENTAR ESSA LINHA: (Rodar o criar_banco.py uma vez e comentar a linha novamente)
-# app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://db_fakepinterest_4fe5_user:ER9gYCdiR8CxXU5i7HHrpv58HavSmsWB@dpg-coupre021fec73bro6l0-a.oregon-postgres.render.com/db_fakepinterest_4fe5"  # External Database URL
+#app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://db_fakepinterest_ef9p_user:E5zUXwaxveETPzMKi2iLur2MKZCYQ3gb@dpg-cous2i021fec73bsuuv0-a.oregon-postgres.render.com/db_fakepinterest_ef9p"  # External Database URL
 
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"  # configuração para criação do banco
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")  # configuração para criação do banco
 app.config["SECRET_KEY"] = "204fd3b542111b5941ee62257eef7ec1" # nosso aplicativo vai usar a chave de segurança como referência pra garantir a segurança do app
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"  # define onde as fotos serão armazenadas
 
