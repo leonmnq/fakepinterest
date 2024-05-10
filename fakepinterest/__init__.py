d69d0b7041ced370e0c586fbfa5dd92760017c05
leonmnq <leon.mnq@gmail.com>
2024-05-10 02:12:34 -0300

    database

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -9,12 +9,16 @@ import os
 app = Flask(__name__)  # app nada mais é, que o nosso site, nossa aplicação
 
 # condição para ambiente online e ambiente local
-if os.getenv("DEBUG") == 0:  # significa que não está em um ambiente de debug OBS: necessário criar a variável de ambiente debug com valor 0 lá no sistema do render
-    link_banco = os.getenv("DATABASE_URL")  # o link será esse, significa que está no ambiente online
-else:
-    link_banco = "sqlite:///comunidade.db"  # utiliza o banco de dados local, offline
+# if os.getenv("DEBUG") == 0:  # significa que não está em um ambiente de debug OBS: necessário criar a variável de ambiente debug com valor 0 lá no sistema do render
+#     link_banco = os.getenv("DATABASE_URL")  # o link será esse, significa que está no ambiente online
+# else:
+#     link_banco = "sqlite:///comunidade.db"  # utiliza o banco de dados local, offline
 
-app.config["SQLALCHEMY_DATABASE_URI"] = link_banco  # configuração para criação do banco
+# PARA CRIAR AS TABELAS DO BANCO DE DADOS LÁ NO BANCO POSTEGRESQL DO RENDER, DESCOMENTAR ESSA LINHA: (Rodar o criar_banco.py uma vez e comentar a linha novamente)
+# app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://db_fakepinterest_4fe5_user:ER9gYCdiR8CxXU5i7HHrpv58HavSmsWB@dpg-coupre021fec73bro6l0-a.oregon-postgres.render.com/db_fakepinterest_4fe5"  # External Database URL
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")  # configuração para criação do banco
 app.config["SECRET_KEY"] = "204fd3b542111b5941ee62257eef7ec1" # nosso aplicativo vai usar a chave de segurança como referência pra garantir a segurança do app
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"  # define onde as fotos serão armazenadas
 
