839c5ac1c910d403942247fecd7c92677108b5f5
leonmnq <leon.mnq@gmail.com>
2024-05-09 23:39:16 -0300

    debug false

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from fakepinterest import app
 # Essa linha abaixo diz pra nosso código: se eu executar o arquivo main.py, é pra rodar o app.run(). Mas se esse
 # main.py for importado de outro arquivo, NÃO é pra rodar o app.run()
 if __name__ == "__main__":
-    app.run(debug=True)   # Vai colocar nosso site no ar # debug=True - todas as alterações que fizermos no código,
+    app.run(debug=False)   # Vai colocar nosso site no ar # debug=True - todas as alterações que fizermos no código,
     # serão automaticamente atualizadas no site no ar
 
